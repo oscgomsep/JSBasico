e8bbdef945fad65eded2ffa673b154c78e75b7b8
oscgomsep <oscargs679@gmail.com>
2020-07-23 22:58:21 -0500

    Mejoras del programa de cuentas

--- JS/JS26.js
+++ JS/JS26.js
@@ -1,30 +1,38 @@
 //desarrollo del Ejercicio de cuenta bancaria
+// definicion del objeto
 var cuenta={
     titular:"Oscar",
-    saldo: 0 ,
+    saldo:0,
 
 //  solicitud de información al usuario
     ingresar: function (ingreso){
-        console.log ("Se ha consignado:" + ingreso + "euros");
-        return this.saldo+ingreso;
+        console.log("Se ha consignado: $" + ingreso);
+        return this.saldo += ingreso;
     },
     retirar: function (egreso){
-       console.log("Se ha descontado:"+ egreso);
-       return this.saldo-egreso
+       console.log("Se ha descontado: $"+ egreso);
+       return this.saldo -= egreso;
     },
     // informe de saldo
-    informe: function(estado){
+    informe: function (){
      console.log("Nombre del Titular:", this.titular);
-     console.log("El saldo actual de su cuenta es:", this.saldo);
+     console.log("El saldo actual de su cuenta es: $", this.saldo);
     }
- }
+ };
 
 //  información a pantalla
  console.log (cuenta.titular);
  console.log (cuenta.saldo);
- console.log ("-----------");
- let ingreso=prompt("Digite el valor a consignar");
+ console.log ("**********************");
+ console.log ("**********************");
+ let ingreso=prompt("Cuanto va a consignar?");
  cuenta.ingresar(ingreso);
- let retiro=prompt("Digite el valor a retirar");
+ let retiro=prompt("Cuanto va a retirar?");
+ console.log ("**********************");
+ console.log ("**********************");
  cuenta.retirar(retiro);
+ console.log ("**********************");
+ console.log ("**********************");
  cuenta.informe();
+ console.log ("**********************");
+ console.log ("**********************");

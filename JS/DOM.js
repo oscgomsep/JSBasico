78e2cde5d3a5807fb9960601ff0e8092f98fceb3
oscgomsep <oscargs679@gmail.com>
2020-07-24 20:05:37 -0500

    Eliminación del JS externo para el DOM

--- JS/DOM.js
+++
@@ -1,7 +0,0 @@
-var refer = document.getElementsByTagName("a");
-var num = refer.length;
-var penRefer = refer[refer.length - 2];
-var con
-console.log("Hay "+ num + " de referencias en el documento")
-console.log("El último enlace es a la dirección " + penRefer)
-for
